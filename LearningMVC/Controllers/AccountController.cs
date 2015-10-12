9cd58874b50af882b501ee017fbbfe0d7abfcb41
Ody Mbegbu <odytrice@gmail.com>
2015-10-12 13:58:32 +0400

    Started Forms Authentication

---
+++ LearningMVC/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using LearningMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            return View(model);
+        }
+    }
+}
